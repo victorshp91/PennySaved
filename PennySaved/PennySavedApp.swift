3da28de8d3e879aa897cb4fccedb75f7e52048b9
Victor Saint Hilaire <victorshp30@gmail.com>
2024-08-12 20:23:12 -0400

    Initial Commit

---
+++ PennySaved/PennySavedApp.swift
@@ -0,0 +1,20 @@
+//
+//  PennySavedApp.swift
+//  PennySaved
+//
+//  Created by Victor Saint Hilaire on 8/12/24.
+//
+
+import SwiftUI
+
+@main
+struct PennySavedApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
